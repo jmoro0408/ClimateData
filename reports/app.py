0ed1b07b9bb0af2c182235ad5f82e5fd13739969
jmoro0408 <james.moro.11@gmail.com>
2021-11-13 18:00:09 -0800

    added figure file

--- reports/app.py
+++ reports/app.py
@@ -2,42 +2,83 @@ import dash
 from dash import dcc
 from dash import html
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 app = dash.Dash(__name__)
 
-colors = {"background": "#111111", "text": "#7FDBFF"}
+colors = {"background": "#ffffff", "text": "#000000"}
+fonts = {"font-family": ["Arial", "Helvetica", "sans-serif"]}
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame(
-    {
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-    }
+CH4_df = pd.read_pickle(
+    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/CH4_df.pkl"
+)
+CO2_df = pd.read_pickle(
+    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/CO2_df.pkl"
+)
+HFC_df = pd.read_pickle(
+    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/HFC_df.pkl"
+)
+N2O_df = pd.read_pickle(
+    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/N2O_df.pkl"
+)
+NF3_df = pd.read_pickle(
+    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/NF3_df.pkl"
+)
+PFC_df = pd.read_pickle(
+    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/PFC_df.pkl"
+)
+SF6_df = pd.read_pickle(
+    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/SF6_df.pkl"
 )
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-fig.update_layout(
+
+
+
+ghg_fig_country = "United Kingdom"
+
+ghg_fig = go.Figure(
+    data=go.Scatter(
+        x=CH4_df[CH4_df["Country or Area"] == ghg_fig_country]["Year"],
+        y=CH4_df[CH4_df["Country or Area"] == ghg_fig_country]["Value"],
+    )
+)
+
+
+ghg_fig.update_layout(
     plot_bgcolor=colors["background"],
     paper_bgcolor=colors["background"],
     font_color=colors["text"],
+    title_text=f"Methane Emissions - {ghg_fig_country}",
+    xaxis_title='Year',
+    yaxis_title='Temperature (degrees F)')
+    font_size=18,
 )
+
 
 app.layout = html.Div(
     style={"backgroundColor": colors["background"]},
     children=[
         html.H1(
-            children="Hello Dash",
-            style={"textAlign": "center", "color": colors["text"]},
+            children="Paris Agreement Update",
+            style={
+                "textAlign": "left",
+                "color": colors["text"],
+                "font-family": fonts["font-family"],
+            },
         ),
         html.Div(
-            children="Dash: A web application framework for your data.",
-            style={"textAlign": "center", "color": colors["text"]},
+            children="How far are we from the target?",
+            style={
+                "textAlign": "left",
+                "color": colors["text"],
+                "font-family": fonts["font-family"],
+            },
         ),
-        dcc.Graph(id="example-graph-2", figure=fig),
+        dcc.Graph(id="ghg-line-plot", figure=ghg_fig),
     ],
 )
 
---
+++ reports/figures/ghg_figure.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+CH4_df = pd.read_pickle(
+    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/CH4_df.pkl"
+)
+CO2_df = pd.read_pickle(
+    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/CO2_df.pkl"
+)
+HFC_df = pd.read_pickle(
+    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/HFC_df.pkl"
+)
+
+NF3_df = pd.read_pickle(
+    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/NF3_df.pkl"
+)
+PFC_df = pd.read_pickle(
+    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/PFC_df.pkl"
+)
+SF6_df = pd.read_pickle(
+    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/SF6_df.pkl"
+)
+N2O_df = pd.read_pickle(
+    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/N20_df.pkl"
+)
