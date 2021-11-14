068b68fe98375a78af4e95a3461da4217de5a07c
jmoro0408 <james.moro.11@gmail.com>
2021-11-13 18:26:51 -0800

    split ghg figure into own file

--- reports/app.py
+++ reports/app.py
@@ -4,6 +4,7 @@ from dash import html
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+from figures.ghg_figure import ghg_fig
 
 app = dash.Dash(__name__)
 
@@ -12,51 +13,6 @@ fonts = {"font-family": ["Arial", "Helvetica", "sans-serif"]}
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-CH4_df = pd.read_pickle(
-    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/CH4_df.pkl"
-)
-CO2_df = pd.read_pickle(
-    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/CO2_df.pkl"
-)
-HFC_df = pd.read_pickle(
-    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/HFC_df.pkl"
-)
-N2O_df = pd.read_pickle(
-    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/N2O_df.pkl"
-)
-NF3_df = pd.read_pickle(
-    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/NF3_df.pkl"
-)
-PFC_df = pd.read_pickle(
-    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/PFC_df.pkl"
-)
-SF6_df = pd.read_pickle(
-    "/Users/jamesmoro/Documents/Python/ClimateData/data/interim/SF6_df.pkl"
-)
-
-
-
-
-
-ghg_fig_country = "United Kingdom"
-
-ghg_fig = go.Figure(
-    data=go.Scatter(
-        x=CH4_df[CH4_df["Country or Area"] == ghg_fig_country]["Year"],
-        y=CH4_df[CH4_df["Country or Area"] == ghg_fig_country]["Value"],
-    )
-)
-
-
-ghg_fig.update_layout(
-    plot_bgcolor=colors["background"],
-    paper_bgcolor=colors["background"],
-    font_color=colors["text"],
-    title_text=f"Methane Emissions - {ghg_fig_country}",
-    xaxis_title='Year',
-    yaxis_title='Temperature (degrees F)')
-    font_size=18,
-)
 
 
 app.layout = html.Div(
--- reports/figures/ghg_figure.py
+++ reports/figures/ghg_figure.py
@@ -1,24 +1,59 @@
 import pandas as pd
+import plotly.graph_objects as go
 
-CH4_df = pd.read_pickle(
-    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/CH4_df.pkl"
-)
-CO2_df = pd.read_pickle(
-    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/CO2_df.pkl"
-)
-HFC_df = pd.read_pickle(
-    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/HFC_df.pkl"
+colors = {"background": "#ffffff", "text": "#000000"}
+fonts = {"font-family": ["Arial", "Helvetica", "sans-serif"]}
+
+ROOT_DIR = r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/"
+DF_NAMES = (
+    "CH4_df",
+    "CO2_df",
+    "HFC_df",
+    "NF3_df",
+    "PFC_df",
+    "SF6_df",
+    "N20_df",
 )
 
-NF3_df = pd.read_pickle(
-    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/NF3_df.pkl"
+CH4_df = pd.read_pickle(ROOT_DIR + DF_NAMES[0] + ".pkl")
+CO2_df = pd.read_pickle(ROOT_DIR + DF_NAMES[1] + ".pkl")
+HFC_df = pd.read_pickle(ROOT_DIR + DF_NAMES[2] + ".pkl")
+NF3_df = pd.read_pickle(ROOT_DIR + DF_NAMES[3] + ".pkl")
+PFC_df = pd.read_pickle(ROOT_DIR + DF_NAMES[4] + ".pkl")
+SF6_df = pd.read_pickle(ROOT_DIR + DF_NAMES[5] + ".pkl")
+N20_df = pd.read_pickle(ROOT_DIR + DF_NAMES[6] + ".pkl")
+
+ghg_gas_name_dict = {
+    "Methane": CH4_df,
+    "Carbon Dioxide": CO2_df,
+    "Hydrofluorocarbons": HFC_df,
+    "Nitrogen trifluoride": NF3_df,
+    "Perfluorocarbon": PFC_df,
+    "Sulfur Hexafluoride": SF6_df,
+    "Nitrous Oxide": N20_df,
+}
+
+ghg_fig_country = "Germany"
+ghg_df_selection_name = "Perfluorocarbon"
+ghg_df_selection_df = ghg_gas_name_dict.get(ghg_df_selection_name)
+
+ghg_fig = go.Figure(
+    data=go.Scatter(
+        x=ghg_df_selection_df[
+            ghg_df_selection_df["Country or Area"] == ghg_fig_country
+        ]["Year"],
+        y=ghg_df_selection_df[
+            ghg_df_selection_df["Country or Area"] == ghg_fig_country
+        ]["Value"],
+    )
 )
-PFC_df = pd.read_pickle(
-    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/PFC_df.pkl"
-)
-SF6_df = pd.read_pickle(
-    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/SF6_df.pkl"
-)
-N2O_df = pd.read_pickle(
-    r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/N20_df.pkl"
+
+ghg_fig.update_layout(
+    plot_bgcolor=colors["background"],
+    paper_bgcolor=colors["background"],
+    font_color=colors["text"],
+    title_text=f"{ghg_df_selection_name} Emissions - {ghg_fig_country}",
+    xaxis_title="Year",
+    yaxis_title="Temperature (degrees F)",
+    font_size=18,
 )
