ba07e42b059d7e9553281d0593f9369f87d3038a
jmoro0408 <james.moro.11@gmail.com>
2021-11-27 18:36:59 +0000

    updated to combned dataframe

--- reports/figures/ghg_figure.py
+++ reports/figures/ghg_figure.py
@@ -4,50 +4,50 @@ import plotly.graph_objects as go
 colors = {"background": "#ffffff", "text": "#000000"}
 fonts = {"font-family": ["Arial", "Helvetica", "sans-serif"]}
 
-ROOT_DIR = r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/"
-DF_NAMES = (
-    "CH4_df",
-    "CO2_df",
-    "HFC_df",
-    "NF3_df",
-    "PFC_df",
-    "SF6_df",
-    "N20_df",
-)
+# ROOT_DIR = r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/"
+# DF_NAMES = (
+#     "CH4_df",
+#     "CO2_df",
+#     "HFC_df",
+#     "NF3_df",
+#     "PFC_df",
+#     "SF6_df",
+#     "N20_df",
+# )
 
-CH4_df = pd.read_pickle(ROOT_DIR + DF_NAMES[0] + ".pkl")
-CO2_df = pd.read_pickle(ROOT_DIR + DF_NAMES[1] + ".pkl")
-HFC_df = pd.read_pickle(ROOT_DIR + DF_NAMES[2] + ".pkl")
-NF3_df = pd.read_pickle(ROOT_DIR + DF_NAMES[3] + ".pkl")
-PFC_df = pd.read_pickle(ROOT_DIR + DF_NAMES[4] + ".pkl")
-SF6_df = pd.read_pickle(ROOT_DIR + DF_NAMES[5] + ".pkl")
-N20_df = pd.read_pickle(ROOT_DIR + DF_NAMES[6] + ".pkl")
+combined_df_dir = r"/Users/jamesmoro/Documents/Python/ClimateData/data/interim/Combined_ghg_dataframe.pkl"
+combined_df = pd.read_pickle(combined_df_dir)
+
+# CH4_df = pd.read_pickle(ROOT_DIR + DF_NAMES[0] + ".pkl")
+# CO2_df = pd.read_pickle(ROOT_DIR + DF_NAMES[1] + ".pkl")
+# HFC_df = pd.read_pickle(ROOT_DIR + DF_NAMES[2] + ".pkl")
+# NF3_df = pd.read_pickle(ROOT_DIR + DF_NAMES[3] + ".pkl")
+# PFC_df = pd.read_pickle(ROOT_DIR + DF_NAMES[4] + ".pkl")
+# SF6_df = pd.read_pickle(ROOT_DIR + DF_NAMES[5] + ".pkl")
+# N20_df = pd.read_pickle(ROOT_DIR + DF_NAMES[6] + ".pkl")
 
 
 ghg_gas_name_dict = {
-    "Methane": CH4_df,
-    "Carbon Dioxide": CO2_df,
-    "Hydrofluorocarbons": HFC_df,
-    "Nitrogen trifluoride": NF3_df,
-    "Perfluorocarbon": PFC_df,
-    "Sulfur Hexafluoride": SF6_df,
-    "Nitrous Oxide": N20_df,
+    "Methane": "CH4 Emissions",
+    "Carbon Dioxide": "CO2 Emissions",
+    "Hydrofluorocarbons": "HFC Emissions",
+    "Nitrogen trifluoride": "NF3 Emissions",
+    "Perfluorocarbon": "PFC Emissions",
+    "Sulfur Hexafluoride": "SF6 Emissions",
+    "Nitrous Oxide": "N20 Emissions",
+    "Combined Total": "Combined Total",
 }
 
 
 ghg_fig_country = "Australia"
 ghg_df_selection_name = "Methane"
-ghg_df_selection_df = ghg_gas_name_dict.get(ghg_df_selection_name)
+ghg_selection = ghg_gas_name_dict.get(ghg_df_selection_name)
 
-ghg_countries = ghg_df_selection_df["Country or Area"].unique()
+ghg_countries = combined_df["Country"].unique()
 ghg_fig = go.Figure(
     data=go.Scatter(
-        x=ghg_df_selection_df[
-            ghg_df_selection_df["Country or Area"] == ghg_fig_country
-        ]["Year"],
-        y=ghg_df_selection_df[
-            ghg_df_selection_df["Country or Area"] == ghg_fig_country
-        ]["Value"],
+        x=combined_df[combined_df["Country"] == ghg_fig_country]["Year"],
+        y=combined_df[combined_df["Country"] == ghg_fig_country][ghg_selection],
     )
 )
 
